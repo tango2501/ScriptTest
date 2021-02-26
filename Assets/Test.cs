adad8cf6d8fdb56ae1275cb52501989e09c611ca
tango2501 <79126030+tango2501@users.noreply.github.com>
2021-02-26 12:58:35 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //変数を初期化する
+        int a = 3;
+        int b = 4;
+        //変数aと変数bの合計値を変数cに代入する
+        int c = a + b;
+        //変数を表示する
+        Debug.Log(c);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
