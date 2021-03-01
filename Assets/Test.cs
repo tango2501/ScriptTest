1dec9f7b2e8d291c69f95ab42107dc0b5e6db4a4
tango2501 <79126030+tango2501@users.noreply.github.com>
2021-03-01 17:59:15 +0900

    L4C8.1完了時
    引数も返り値もない関数完了時 保存

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+
+    // 「Hello, UnityChan」と表示する巻数
+    void Hello()
+    {
+        Debug.Log("Hello, UnityChan");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        // 配列を初期化する
-        int[] points = { 30, 20, 50, 10, 80, 15, 60, 100 };
-
-        // 配列の要素数のぶんだけ処理を繰り返す
-        for (int i = 0; i < points.Length; i++)
-        {
-            // 配列の要素が50以上の場合
-            if (points[i] >= 50)
-                // 配列の要素を表示する
-                Debug.Log(points[i]);
-        }
+        // Hello関数を呼び出す
+        Hello ();
+        
     }
 
     // Update is called once per frame
