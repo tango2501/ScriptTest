29b7d21ecf9291375b629f7df2017bf77b288d25
tango2501 <79126030+tango2501@users.noreply.github.com>
2021-03-01 17:26:06 +0900

    L4C6完了時
    for文完了時 保存

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,14 +7,11 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // 変数aを3で初期化する
-        int a = 3;
-        if( a == 3 )
+        // 5回処理を繰り返す
+        for (int i = 0; i < 5; i++)
         {
-            //変数aが3の場合、変数bを5で初期化する
-            int b = 5;
+            Debug.Log (i);
         }
-        Debug.Log(b);
     }
 
     // Update is called once per frame
