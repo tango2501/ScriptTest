d4b84c12aae299fb37520a7199df767c7a5ef429
tango2501 <79126030+tango2501@users.noreply.github.com>
2021-03-02 17:30:28 +0900

    L4課題途中経過
    発展課題保存
    コンソールに表示する部分は完成したが、趣旨を間違えている気がするので変更する。
    start内でfor文を使って関数を10回呼び出して、その関数の中でmpを減らしていけばいいのではないか。そしてその呼び出し先で条件分岐をして、mpが足りない場合は"使えない"を返すようにできないか。

---
+++ Assets/forkadai.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;//Array.Reverseを使用するためのもの
+
+
+public class forkadai : MonoBehaviour
+{
+    // 配列を初期化する
+    int[] points = new int[] { 10, 20, 30, 40, 50 };
+    // Start is called before the first frame update
+    void Start()
+    {
+        // for文を使って配列の要素を順に表示する
+        for (int i = 0; i < points.Length; i++)
+        {
+            Debug.Log(points[i]);
+
+        }
+        // for文を使って配列の要素を逆順に表示する
+        Array.Reverse(points);// 配列の要素を逆順にする
+        for (int i = 0; i < points.Length; i++)
+        {
+            Debug.Log(points[i]);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/hattenkadai.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss
+{
+    private int hp = 100;//体力
+    private int power = 25;//攻撃力
+    private int mp = 53;//MP
+    private int zanmp;
+    private int syouhimp = 5;
+
+    //攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+    //防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+    public void Magic()
+    {
+        if (mp > 5)
+        {
+
+            while (mp > 5)
+            {
+                mp -= syouhimp;
+                Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+            }
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+
+        
+    }
+}
+public class hattenkadai : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        //攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+        //魔法攻撃用の関数を呼び出す
+        lastboss.Magic();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
+//呼び出しをfor文にして中身でカウントして条件式をつくるif文
